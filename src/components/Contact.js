f33cc39a3074d451a1a9eb7d92f6b44e1a6c263e
Joseph Yim <yimsjoseph@knights.ucf.edu>
2022-01-07 03:59:32 -0500

    Updates and adds new files

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { motion } from "framer-motion"
+import "../css/Contact.css"
+
+export default function Contact() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
