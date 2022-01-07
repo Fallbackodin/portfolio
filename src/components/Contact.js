6dd14af59fd21e97acf3a60e31f69f914dd23ce7
Joseph Yim <yimsjoseph@knights.ucf.edu>
2022-01-07 14:27:12 -0500

    Adds animation to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,19 +5,39 @@ import "../css/Contact.css"
 export default function Contact() {
     return (
         <div className="contact">
-            <h1 className="contact-header">Contact Me</h1>
-            <div className="contact-row">
+            <motion.h1 className="contact-header"
+            initial="hidden"
+            whileInView="visible"
+            transition={{ duration: 1.5 }}
+            viewport={{ once: true }}
+            variants={{ visible: {opacity: 1, scale: 1, y: ["50%", "0%"]}, hidden: {opacity: 0, scale: 1}, }}>Contact Me</motion.h1>
+            <motion.div className="contact-row"
+            initial="hidden"
+            whileInView="visible"
+            transition={{ duration: 1.5 }}
+            viewport={{ once: true }}
+            variants={{ visible: {opacity: 1, scale: 1, y: ["50%", "0%"]}, hidden: {opacity: 0, scale: 1} }}>
                 <div className="contact-column">
                     <input type="text" placeholder="Name" className="contact-upper-input"/>
                 </div>
                 <div className="contact-column">
                     <input type="text" placeholder="Email" className="contact-upper-input"/>
                 </div>
-            </div>
-            <div className="contact-message">
+            </motion.div>
+            <motion.div className="contact-message"
+            initial="hidden"
+            whileInView="visible"
+            transition={{ duration: 1.5 }}
+            viewport={{ once: true }}
+            variants={{ visible: {opacity: 1, scale: 1, y: ["50%", "0%"]}, hidden: {opacity: 0, scale: 1}, }}>
                 <textarea type="text" placeholder="Message" className="contact-message-input"></textarea>
-            </div>
-            <motion.div className="contact-submit-button">
+            </motion.div>
+            <motion.div className="contact-submit-button"
+            initial="hidden"
+            whileInView="visible"
+            transition={{ duration: 1.5 }}
+            viewport={{ once: true }}
+            variants={{ visible: {opacity: 1, scale: 1, y: ["50%", "0%"]}, hidden: {opacity: 0, scale: 1}, }}>
                 <p className="contact-submit-text">Send Email!</p>
             </motion.div>
         </div>
